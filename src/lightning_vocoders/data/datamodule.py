026600bd108e52004086fe29004f75a4b9fb649e
Wataru Nakata <wataru9871@gmail.com>
2023-06-06 14:20:48 +0900

    reverted dataset to webdataset

--- src/lightning_vocoders/data/datamodule.py
+++ src/lightning_vocoders/data/datamodule.py
@@ -19,25 +19,33 @@ class VocoderDataModule(lightning.LightningDataModule):
         self.cfg = cfg
 
     def setup(self, stage: str):
-        dataset = hydra.utils.instantiate(self.cfg.preprocess.preprocess_dataset)
-        self.train_dataset,self.val_dataset = random_split(dataset,[0.9,0.1])
+        self.train_dataset = (
+            wds.WebDataset(self.cfg.data.train_dataset_path)
+            .shuffle(1000)
+            .decode(wds.torch_audio)
+        )
+        self.val_dataset = wds.WebDataset(self.cfg.data.val_dataset_path).decode(
+            wds.torch_audio
+        )
 
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,
             batch_size=self.cfg.data.train_batch_size,
             collate_fn=lambda batch: self.collate_fn(
-                batch, self.cfg.model.train_segment_size
+                batch, self.cfg.data.segment_size.train
             ),
-            num_workers=0,
+            num_workers=8,
         )
 
     def val_dataloader(self):
         return DataLoader(
             self.val_dataset,
             batch_size=self.cfg.data.val_batch_size,
-            collate_fn=self.collate_fn,
-            num_workers=0,
+            collate_fn=lambda batch: self.collate_fn(
+                batch, self.cfg.data.segment_size.val
+            ),
+            num_workers=8,
         )
 
     @torch.no_grad()
@@ -46,38 +54,50 @@ class VocoderDataModule(lightning.LightningDataModule):
         outputs = dict()
         if segment_size != -1:
             cropped_speeches = []
+            input_features = []
             for sample in batch:
-                file_name, (wav,sr) = sample
-                audio_len = wav.size(1)
-                if audio_len >= segment_size:
-                    speech_start = random.randint(
-                        0, audio_len - segment_size - 1
+                wav = sample["resampled_speech.pth"]
+                input_feature= sample[self.cfg.data.target_feature.key]
+                feature_len = input_feature.size(0)
+                if feature_len > (segment_size+1):
+                    feature_start = random.randint(
+                        0, feature_len - segment_size - 1
                     )
+                    feature_end = segment_size + feature_start
+                    speech_start_sec = feature_start / self.cfg.data.target_feature.samples_per_sec + self.cfg.data.target_feature.bias
+                    speech_end_sec = (feature_start + segment_size) / self.cfg.data.target_feature.samples_per_sec + self.cfg.data.target_feature.bias
                     cropped_speeches.append(
                         wav.squeeze()[
-                            speech_start : speech_start+segment_size
+                            int(speech_start_sec * self.cfg.sample_rate) : int(speech_end_sec * self.cfg.sample_rate)
+                        ]
+                    )
+                    input_features.append(
+                        input_feature[
+                            feature_start:feature_end
                         ]
                     )
                 else:
                     cropped_speeches.append(wav.squeeze())
+                    input_features.append(
+                        input_feature
+                    )
             outputs["resampled_speech.pth"] = pad_sequence(
                 cropped_speeches, batch_first=True
             )
+            outputs["input_feature"] = pad_sequence(
+                input_features, batch_first=True
+            )
         else:
             outputs["resampled_speech.pth"] = pad_sequence(
-                [b[1][0].squeeze() for b in batch], batch_first=True
+                [b["resampled_speech.pth"].squeeze() for b in batch], batch_first=True
+            )
+            outputs["input_feature"] = pad_sequence(
+                [b[self.cfg.data.target_feature.key].squeeze() for b in batch], batch_first=True
             )
         
         outputs["wav_lens"] = torch.tensor(
-            [b[1][0].size(1) for b in batch]
+            [b["resampled_speech.pth"].size(0) for b in batch]
         )
 
-        outputs["filenames"] = [b[0] for b in batch]
+        outputs["filenames"] = [b["__key__"] for b in batch]
         return outputs
-
-    def calc_spectrogram(self, waveform: torch.Tensor):
-        magspec = self.spec_module(waveform)
-        melspec = self.mel_scale(magspec)
-        logmelspec = torch.log(torch.clamp_min(melspec, 1.0e-5) * 1.0).to(torch.float32)
-        energy = torch.norm(magspec, dim=0)
-        return logmelspec, energy
