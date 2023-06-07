19eb23316ba746359ae15c76e3cb305c6a57e4d6
Wataru Nakata <wataru9871@gmail.com>
2023-06-07 17:46:35 +0900

    changed config for supporting more datasets

--- src/lightning_vocoders/preprocessor/dataset/glob_wav_dataset.py
+++ src/lightning_vocoders/preprocessor/dataset/glob_wav_dataset.py
@@ -3,12 +3,18 @@ from torch.utils.data.dataset import Dataset
 import torchaudio
 from pathlib import Path
 import random
+import string
 
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(length))
 
 class GlobWavDataset(Dataset):
-    def __init__(self, root, pattern, shuffled: bool = True) -> None:
-        self.root = Path(root)
-        self.wav_files = list(self.root.glob(pattern))
+    def __init__(self, roots, patterns, shuffled: bool = True) -> None:
+        self.wav_files = []
+        for root,pattern in zip(roots,patterns):
+            self.root = Path(root)
+            self.wav_files.extend(list(self.root.glob(pattern)))
         if shuffled:
             random.shuffle(self.wav_files)
 
@@ -18,4 +24,4 @@ class GlobWavDataset(Dataset):
 
     def __getitem__(self,idx):
         wav_path = self.wav_files[idx]
-        return wav_path.stem, torchaudio.load(wav_path),str(wav_path)
+        return wav_path.stem + generate_random_string(5), torchaudio.load(wav_path),str(wav_path)
