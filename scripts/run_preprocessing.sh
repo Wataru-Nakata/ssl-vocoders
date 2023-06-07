35c8c11a62914b344ac25c0a31b9bb199a9e5df1
Wataru Nakata <wataru9871@gmail.com>
2023-06-07 20:41:15 +0900

    added shell script to perform preprocessing

---
+++ scripts/run_preprocessing.sh
@@ -0,0 +1,6 @@
+python3 src/preprocess.py preprocess=wav2vec2-base
+python3 src/preprocess.py preprocess=wav2vec2-large
+python3 src/preprocess.py preprocess=wavlm-base
+python3 src/preprocess.py preprocess=wavlm-large
+python3 src/preprocess.py preprocess=hubert-base
+python3 src/preprocess.py preprocess=hubert-large
