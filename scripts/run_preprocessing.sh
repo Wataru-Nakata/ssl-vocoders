4da0117d02e4b102fba70be43f0252a189d2c193
Wataru Nakata <wataru9871@gmail.com>
2023-06-07 21:53:11 +0900

    updated path

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -1,6 +1,6 @@
-python3 src/preprocess.py preprocess=wav2vec2-base
-python3 src/preprocess.py preprocess=wav2vec2-large
-python3 src/preprocess.py preprocess=wavlm-base
-python3 src/preprocess.py preprocess=wavlm-large
-python3 src/preprocess.py preprocess=hubert-base
-python3 src/preprocess.py preprocess=hubert-large
+python3 src/preprocess.py preprocess=wav2vec2-base > wavlm_base.log &
+python3 src/preprocess.py preprocess=wav2vec2-large > wavlm_large.log &
+python3 src/preprocess.py preprocess=wavlm-base > wavlm_base.log &
+python3 src/preprocess.py preprocess=wavlm-large > wavlm_large.log &
+python3 src/preprocess.py preprocess=hubert-base > hubert_base.log &
+python3 src/preprocess.py preprocess=hubert-large > hubert_large.log  
