35d7ed6e101a9f11e8f05742f7a4eb257d9be497
Wataru Nakata <wataru9871@gmail.com>
2023-06-13 13:19:23 +0900

    fixed typo

--- scripts/run_training_wav2vec2_base.sh
+++ scripts/run_training_wav2vec2_base.sh
@@ -9,4 +9,4 @@ module load cuda/12.1/12.1.1
 module load cudnn/8.9/8.9.2
 module load nccl/2.18/2.18.1-1
 source venv/bin/activate
-python3 src/train.py preprocess=wav2vec2-base model=hifigan_ssl data=wav2vec2_base data.train_dataset_path=/scratch/acc12576tt/wav2vec2_base/wav2vec2_base-train-{000000..000119}.tar.gz data.val_dataset_path=/scratch/acc12576tt/wav2vec2_base/wav2vec2_base-val-{000000..000001}.tar.gz
+python3 src/train.py preprocess=wav2vec2_base model=hifigan_ssl data=wav2vec2_base data.train_dataset_path=/scratch/acc12576tt/wav2vec2_base/wav2vec2_base-train-{000000..000119}.tar.gz data.val_dataset_path=/scratch/acc12576tt/wav2vec2_base/wav2vec2_base-val-{000000..000001}.tar.gz
