16800006256e54687a6297f19719f275c4871a99
Wataru Nakata <wataru9871@gmail.com>
2023-06-13 11:53:45 +0900

    added seed fixing for reproducibility

--- src/train.py
+++ src/train.py
@@ -5,12 +5,14 @@ import torch
 from omegaconf import DictConfig
 from lightning_vocoders.models.hifigan.lightning_module import HiFiGANLightningModule
 from lightning.pytorch.callbacks import LearningRateMonitor
+from lightning.pytorch import seed_everything
 
 pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
 
 @hydra.main(version_base="1.3", config_name="config", config_path="../config")
 def main(cfg: DictConfig):
+    seed_everything(1234)
     lightning_module = hydra.utils.instantiate(cfg.model.lightning_module, cfg)
     if cfg.compile:
         lightning_module = torch.compile(lightning_module, dynamic=True)
