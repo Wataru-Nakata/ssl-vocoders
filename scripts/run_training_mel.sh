d56a8ea4778b20d8ea889011fe12024e0919e375
Wataru Nakata <wataru9871@gmail.com>
2023-06-13 14:42:00 +0900

    added script for mel

---
+++ scripts/run_training_mel.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#$ -l rt_AG.small=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$-cwd
+source /etc/profile.d/modules.sh
+module load python/3.10/3.10.10
+module load cuda/12.1/12.1.1
+module load cudnn/8.9/8.9.2
+module load nccl/2.18/2.18.1-1
+source venv/bin/activate
+python3 src/train.py preprocess=wav2vec2_base model=hifigan_mel data=mel 
