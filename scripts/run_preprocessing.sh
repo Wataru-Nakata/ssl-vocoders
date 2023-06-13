2c92d7748d300f31d0f69b57e8091d8d0b09e5f6
Wataru Nakata <wataru9871@gmail.com>
2023-06-13 12:01:47 +0900

    added script for preprocessing

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -1,6 +1,6 @@
-python3 src/preprocess.py preprocess=wav2vec2-base > wavlm_base.log &
-python3 src/preprocess.py preprocess=wav2vec2-large > wavlm_large.log &
-python3 src/preprocess.py preprocess=wavlm-base > wavlm_base.log &
-python3 src/preprocess.py preprocess=wavlm-large > wavlm_large.log &
-python3 src/preprocess.py preprocess=hubert-base > hubert_base.log &
+python3 src/preprocess.py preprocess=wav2vec2-base > wav2vec2_base.log 
+python3 src/preprocess.py preprocess=wav2vec2-large > wav2vec2_large.log 
+python3 src/preprocess.py preprocess=wavlm-base > wavlm_base.log 
+python3 src/preprocess.py preprocess=wavlm-large > wavlm_large.log 
+python3 src/preprocess.py preprocess=hubert-base > hubert_base.log 
 python3 src/preprocess.py preprocess=hubert-large > hubert_large.log  
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,7 @@
 import pyrootutils
 import hydra
 from omegaconf import DictConfig
+from lightning.pytorch import seed_everything
 
 pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 from lightning_vocoders.preprocessor.preprocessor import Preprocessor
@@ -8,6 +9,7 @@ from lightning_vocoders.preprocessor.preprocessor import Preprocessor
 
 @hydra.main(version_base="1.3", config_name="config", config_path="../config")
 def main(cfg: DictConfig):
+    seed_everything(1234)
     preprocssor = Preprocessor(cfg=cfg)
     preprocssor.build_from_path()
 
