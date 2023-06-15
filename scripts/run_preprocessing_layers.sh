821b5da6a061f58ff5ac48ace14a233b93e7b048
Wataru Nakata <wataru9871@gmail.com>
2023-06-15 16:01:55 +0900

    added preprocess configs for different layers

---
+++ scripts/run_preprocessing_layers.sh
@@ -0,0 +1,3 @@
+python3 src/preprocess.py preprocess=wav2vec2_base_l3 > wav2vec2_base_l3.log 
+python3 src/preprocess.py preprocess=wav2vec2_base_l6 > wav2vec2_base_l6.log 
+python3 src/preprocess.py preprocess=wav2vec2_base_l9 > wav2vec2_base_l9.log 
