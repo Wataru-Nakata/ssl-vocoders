f9a8e6b7e53943dba08b23396f24d1b30cd6114b
Wataru Nakata <wataru9871@gmail.com>
2023-07-03 23:40:16 +0900

    added configs for l3 and l6 training

---
+++ scripts/run_preprocessing_l3.sh
@@ -0,0 +1,6 @@
+#python3 src/preprocess.py preprocess=wav2vec2_base_l3
+#python3 src/preprocess.py preprocess=hubert_base_l3
+#python3 src/preprocess.py preprocess=wavlm_base_l3
+python3 src/preprocess.py preprocess=wav2vec2_large_l6
+python3 src/preprocess.py preprocess=hubert_large_l6
+python3 src/preprocess.py preprocess=wavlm_large_l6
