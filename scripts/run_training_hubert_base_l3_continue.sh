4037fe63c36deee49fdd164b3e52cf0e1a60cee4
Wataru Nakata <wataru9871@gmail.com>
2023-07-07 11:18:45 +0900

    added continue script

---
+++ scripts/run_training_hubert_base_l3_continue.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#$ -l rt_AG.small=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$-cwd
+source /etc/profile.d/modules.sh
+module load python/3.10/3.10.10
+module load cuda/12.1/12.1.1
+module load cudnn/8.9/8.9.2
+module load nccl/2.18/2.18.1-1
+source venv/bin/activate
+python3 src/train.py preprocess=hubert_base_l3 model=hifigan_ssl data=hubert_base_l3 'train.ckpt_path="/home/acc12576tt/hubert-base-l3/model.ckpt"'
